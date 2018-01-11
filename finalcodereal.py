33a5ee6ec0ca08165ee03d1658b3a21eb45bfd7f
Katie <kwhitehead2@ucvts.tec.nj.us>
2018-01-11 17:15:13 +0000

    first draft

---
+++ finalcodereal.py
@@ -0,0 +1,17 @@
+print("""You are walking alone at night when you hear a fall of footsteps behind you. You turn but see know one. You quicken your pace and continue to glance behind you. Just when you think you must have been just hearing things, you see a shadow. Before you can turn to get a good look your feel a cloth over your mouth and your vision goes black.""")
+
+character = input("What is your name? ")
+
+
+print(f"{character} wakes up in a room blindfolded and tied to a chair. The room is empty and there is a door on the other side of the room. This is your chance to escape. The first thing you have to do is take off your blindfold.")
+
+
+
+answer_1 = "Trenton".lower()
+while question_1 != answer_1:
+    question_1 = input("What is the capitol of New Jersey? ")
+    if question_1!= Trenton:
+        print("Your answer is wrong. You remain in the seat blindfolded. Try again.")
+    else:
+        print("Congragulations! you have now successfully removed your blindfold!")
+
